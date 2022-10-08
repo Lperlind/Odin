27eb401c551875d3fd165cc71527af272b6a19c6
Lucas Perlind <perlindluca@gmail.com>
2022-10-08 16:40:02 +1100

    Bash error return

--- tests/issues/run.sh
+++ tests/issues/run.sh
@@ -6,14 +6,23 @@ pushd build
 ODIN=../../../odin
 COMMON="-collection:tests=../.."
 
+set ERROR_DID_OCCUR=0
+
 set -x
 
 $ODIN test ../test_issue_829.odin  $COMMON -file
 $ODIN test ../test_issue_1592.odin $COMMON -file
 $ODIN test ../test_issue_2087.odin $COMMON -file
-$ODIN test ../test_issue_2113.odin $COMMON -file -debug
+$ODIN build ../test_issue_2113.odin $COMMON -file -debug
 
 set +x
+if [ $retVal -ne 0 ]; then
+    set ERROR_DID_OCCUR=1
+fi
 
 popd
 rm -rf build
+
+if [ $retVal -ne 0 ]; then
+    exit 1
+fi
